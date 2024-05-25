f258d9b45b1ad7acb91cc641235d996d1ba45148
VinayKumarKadirika <167535048+VinayKumarKadirika@users.noreply.github.com>
2024-05-25 22:48:02 +0530

    Update ahb_sequencer.sv

--- TESTBENCH/AHB_AGENT/ahb_sequencer.sv
+++ TESTBENCH/AHB_AGENT/ahb_sequencer.sv
@@ -1,1 +1,17 @@
 
+class ahb_sequencer extends uvm_sequencer#(ahb_transaction);
+   `uvm_component_utils(ahb_sequencer)
+
+   function new(string name = "ahb_sequencer",uvm_component parent);
+      super.new(name,parent);
+   endfunction
+
+   function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+   endfunction
+   
+   function void connect_phase(uvm_phase phase);
+      super.connect_phase(phase);
+   endfunction
+
+endclass
