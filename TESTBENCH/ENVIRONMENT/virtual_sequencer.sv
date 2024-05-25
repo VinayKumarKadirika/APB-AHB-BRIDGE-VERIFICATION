d207ddd6b118ec089194aecb197c4ffc402a9778
VinayKumarKadirika <167535048+VinayKumarKadirika@users.noreply.github.com>
2024-05-25 22:44:00 +0530

    Update virtual_sequencer.sv

--- TESTBENCH/ENVIRONMENT/virtual_sequencer.sv
+++ TESTBENCH/ENVIRONMENT/virtual_sequencer.sv
@@ -1,1 +1,13 @@
 
+class virtual_sequencer extends uvm_sequencer;
+  
+   `uvm_component_utils(virtual_sequencer)
+  
+   ahb_sequencer ahb_sequencer_h;
+   apb_sequencer apb_sequencer_h;
+  
+   function new(string name="virtual_sequencer",uvm_component parent=null);
+      super.new(name,parent);
+   endfunction
+  
+endclass
