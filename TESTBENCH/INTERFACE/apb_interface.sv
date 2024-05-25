a99bb91809bb5d9a61fe3081bdc7bc54238d4b09
VinayKumarKadirika <167535048+VinayKumarKadirika@users.noreply.github.com>
2024-05-25 22:42:51 +0530

    Update apb_interface.sv

--- TESTBENCH/INTERFACE/apb_interface.sv
+++ TESTBENCH/INTERFACE/apb_interface.sv
@@ -1,1 +1,32 @@
 
+interface apb_interface(input bit pclk);
+   
+   logic [3:0] psel;
+   logic penable;
+   logic pwrite;
+   logic [31:0]prdata;
+   logic [31:0]pwdata;
+   logic [31:0]paddr;
+
+clocking apb_driver_cb @(posedge pclk);
+	input penable;
+	input pwrite;
+	input paddr;
+	input pwdata;
+	output prdata;
+	input psel;
+endclocking
+
+clocking apb_monitor_cb @(posedge pclk);
+	input penable;
+	input pwrite;
+	input paddr;
+	input pwdata;
+	input psel;
+	input prdata;
+endclocking
+
+modport apb_driver_mp(clocking apb_driver_cb);
+modport apb_monitor_mp(clocking apb_monitor_cb);
+
+endinterface
