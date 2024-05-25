ad44fcbaacc217a7b60a7c4a47b2ec902e7fa717
VinayKumarKadirika <167535048+VinayKumarKadirika@users.noreply.github.com>
2024-05-25 22:45:33 +0530

    Update apb_sequencer.sv

--- TESTBENCH/APB_AGNET/apb_sequencer.sv
+++ TESTBENCH/APB_AGNET/apb_sequencer.sv
@@ -1,1 +1,16 @@
+class apb_sequencer extends uvm_sequencer#(apb_transaction);
+   `uvm_component_utils(apb_sequencer)
 
+   function new(string name = "apb_sequencer",uvm_component parent);
+      super.new(name,parent);
+   endfunction
+
+   function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+   endfunction
+   
+   function void connect_phase(uvm_phase phase);
+      super.connect_phase(phase);
+   endfunction
+
+endclass
